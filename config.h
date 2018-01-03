a6f4ef7ee25d6765684adf5c0ad6022c24515a25
suddin0 <suddin@student.42.fr>
2018-01-03 16:26:54 +0100

    .info

---
+++ config.h
@@ -0,0 +1,116 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if debugging is enabled. */
+/* #undef DEBUG */
+
+/* Define to 1 if hash is enabled. */
+#define ENABLE_HASH 1
+
+/* Define to 1 if Imlib2 is enabled. */
+#define ENABLE_IMLIB2 1
+
+/* Define to 1 if PAM is enabled. */
+#define ENABLE_PAM 1
+
+/* Define to 1 if passwd is enabled. */
+/* #undef ENABLE_PASSWD */
+
+/* Define to 1 if Xcursor is enabled. */
+/* #undef ENABLE_XCURSOR */
+
+/* Define to 1 if Xpm is enabled. */
+/* #undef ENABLE_XPM */
+
+/* Define to 1 if Xrender is enabled. */
+#define ENABLE_XRENDER 1
+
+/* Define to 1 if you have the <gcrypt.h> header file. */
+#define HAVE_GCRYPT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `crypt' library (-lcrypt). */
+/* #undef HAVE_LIBCRYPT */
+
+/* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
+#define HAVE_LIBGCRYPT 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the `pam' library (-lpam). */
+#define HAVE_LIBPAM 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <security/pam_appl.h> header file. */
+#define HAVE_SECURITY_PAM_APPL_H 1
+
+/* Define to 1 if you have the <shadow.h> header file. */
+/* #undef HAVE_SHADOW_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have X Ext library. */
+#define HAVE_XEXT 1
+
+/* Define to 1 if you have X Misc library. */
+#define HAVE_XMISC 1
+
+/* Name of package */
+#define PACKAGE "alock"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "arkadiusz.bokowy@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "alock"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "alock 2.3.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "alock"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.3.1"
+
+/* Default PAM service. */
+#define PAM_DEFAULT_SERVICE "system-auth"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.3.1"
+
+/* Define to 1 if dunst integration is enabled. */
+#define WITH_DUNST 1
+
+/* Define to 1 if xbacklight integration is enabled. */
+#define WITH_XBLIGHT 1
